a1588466e1218a5c2ebf2f994e26c89124a07d38
Yash Handa <yashhanda7@yahoo.com>
2022-04-22 06:55:35 +0530

    Added funtion to rotate an array with no aditional memory
     Changes to be committed:
    	new file:   Array/Rotate_Array.cpp

---
+++ Array/Rotate_Array.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+void display_container(T start, T end, string seperator = ",")
+{
+    if (start == end)
+    {
+        cout << "AN Empty container....\n";
+        return;
+    }
+    end--;
+    while (start != end)
+    {
+        cout << *start << " " << seperator << " ";
+        start++;
+    }
+    cout << *start << '\n';
+}
+
+void reverse(vector<int>::iterator begin, vector<int>::iterator end)
+{
+    if (begin == end)
+        return;
+
+    end--;
+    while (begin < end)
+    {
+        *begin += *end;
+        *end = *begin - *end;
+        *begin -= *end;
+
+        begin++;
+        end--;
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 8, 10, 12, 15, 17};
+    int rot = 2;
+    vector<int> v(arr, arr + sizeof(arr) / sizeof(arr[0]));
+
+    cout << "Original array: ";
+    display_container(v.begin(), v.end());
+
+    // rotate the first half
+    reverse(v.begin(), v.begin() + rot);
+
+    // rotate the second half
+    reverse(v.begin() + rot, v.end());
+
+    // rotate the entire vector
+    reverse(v.begin(), v.end());
+
+    cout << "Array Rotated by " << rot << ": ";
+    display_container(v.begin(), v.end());
+    return 0;
+}
