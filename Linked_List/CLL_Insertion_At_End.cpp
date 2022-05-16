c3b7d7125d68ff53c91e6c26c79f7286ecca4dc0
Yash Handa <yashhanda7@yahoo.com>
2022-05-16 05:49:49 +0530

    Added member function to Circular_Linked_List class for inserting at the end.
     Changes to be committed:
    	new file:   Linked_List/CLL_Insertion_At_End.cpp

---
+++ Linked_List/CLL_Insertion_At_End.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+
+using std::cout, std::string;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+template <class T>
+class Circular_Linked_List
+{
+public:
+    T *TAIL;
+
+    Circular_Linked_List()
+    {
+        TAIL = NULL;
+    }
+
+    void display(string sep = " -> ")
+    {
+        if (TAIL == NULL)
+        {
+            cout << "An Empty Linked List...\n";
+            return;
+        }
+
+        T *h = TAIL->next;
+
+        while (h != TAIL)
+        {
+            cout << h->data << sep;
+            h = h->next;
+        }
+        cout << h->data << '\n';
+    }
+
+    bool create_dummy(int size = 5, int mod = 10)
+    {
+        if (TAIL != NULL)
+            return false;
+
+        srand(time(0));
+        T *curr = NULL, *h = NULL;
+        for (int i = 1; i <= size; i++)
+        {
+            if (curr == NULL)
+            {
+                curr = new T(rand() % mod);
+                h = curr;
+            }
+            else
+            {
+                curr->next = new T(rand() % mod);
+                curr = curr->next;
+            }
+            TAIL = curr;
+        }
+        TAIL->next = h;
+        return true;
+    }
+
+    bool insert_end(int data)
+    {
+        T *temp = new T(data);
+        if (TAIL == NULL)
+        {
+            TAIL = temp;
+            TAIL->next = TAIL;
+            return true;
+        }
+
+        temp->next = TAIL->next;
+        TAIL->next = temp;
+        TAIL = temp;
+        return true;
+    }
+};
+
+int main()
+{
+    Circular_Linked_List<Node> cll;
+    cll.create_dummy(7);
+
+    cout << "Initial linked list: ";
+    cll.display();
+
+    cll.insert_end(777);
+
+    cout << "Updated linked list: ";
+    cll.display();
+    return 0;
+}
